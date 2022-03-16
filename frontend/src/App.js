cbffa2f6383422744ab50ed760703158b552b478
Vitor Centeio <vitor.centeio@gmail.com>
2022-03-16 20:08:57 +0000

    implement control search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./components/Header";
+import Search from "./components/Search"
 
 
 function App() {
+  const [word, setWord] = useState("");
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word)
+  }
+
   return (
     <div>
       <Header title="Images Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
 }
---
+++ frontend/src/components/Search.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+
+const Search = ({ word, setWord, handleSubmit }) => {
+  return (
+    <Container className="mt-4">
+      <Row className="justify-content-center">
+        <Col xs={12} md={8} lg={6}>
+          <Form onSubmit={ handleSubmit }>
+            <Row className="align-items-center">
+              <Col xs={9} className="my-1">
+                <Form.Control 
+                  id="inlineFormInputSearch"
+                  type="text"
+                  value={word}
+                  onChange={(e) => setWord(e.target.value)}
+                  placeholder="Search for new image..." />
+              </Col>
+              <Col xs="auto" className="my-1">
+                <Button variant="primary" type="submit">Search</Button>
+              </Col>
+            </Row>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  )
+};
+
+export default Search;
