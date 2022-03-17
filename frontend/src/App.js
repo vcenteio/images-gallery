d2b177c3fb5ef4a81d47a96533323975ff9013d2
Vitor Centeio <vitor.centeio@gmail.com>
2022-03-17 16:47:24 +0000

    store images in the state variable "images"
    Images are fetched from the Unsplash API.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,10 @@ const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 function App() {
   const [word, setWord] = useState("");
+  const [images, setImages] = useState([]);
+
+  console.log(images);
+
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
@@ -15,7 +19,7 @@ function App() {
     )
       .then((result) => result.json())
       .then((data) => {
-        console.log(data);
+        setImages([data, ...images]);
       })
       .catch((err) => {
         console.log(err);
