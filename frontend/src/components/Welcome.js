3953e2bd75a4194fabc882531b1fb633e31d57c6
Vitor Centeio <vitor.centeio@gmail.com>
2022-03-18 12:03:44 +0000

    feat(Welcome.js): improve Welcome component style
    Style based on deprecated Bootstrap Jobotron component

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -1,7 +1,23 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
 
 function Welcome() {
-  return <h2>Welcome to the Images Gallery</h2>;
+  return (
+    <main className="p-5 mb-4 bg-light rounded-3">
+      <div className="container-fluid py-5">
+        <h1 className="display-5 fw-bold">Images Gallery</h1>
+        <p className="col-md-8 fs-4">
+          This is a simple application that search for photos using the Unsplash
+          API. In order to start, enter any search term in the input field.
+        </p>
+        <p>
+          <Button variant="primary" href="https://unsplash.com" target="_blank">
+            Checkout Unsplash
+          </Button>
+        </p>
+      </div>
+    </main>
+  );
 }
 
 export default Welcome;
