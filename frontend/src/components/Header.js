8c873b20b73ce1410fdb60e07bbf2f748343347d
Vitor Centeio <vitor.centeio@gmail.com>
2022-03-18 11:28:57 +0000

    fix(Header.js): make title the Logo alt text

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -11,7 +11,7 @@ const Header = ({ title }) => {
   return (
     <Navbar style={navbarStyle} variant="light">
       <Container>
-        <Logo style={{ maxWidth: "20rem", maxHeight: "3rem" }} />
+        <Logo alt={title} style={{ maxWidth: "20rem", maxHeight: "3rem" }} />
       </Container>
     </Navbar>
   );
