931c12a03b932abd7377620d5d183672087836b9
Vitor Centeio <vitor.centeio@gmail.com>
2022-03-28 19:44:02 +0100

    feat(api): implement /new-image API endpoint

---
+++ api/main.py
@@ -0,0 +1,24 @@
+import os
+
+from flask import Flask, request
+import requests as rq
+from dotenv import load_dotenv
+
+load_dotenv("./.env.local")
+
+UNSPLASH_URL = "https://api.unsplash.com/photos/random"
+UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY")
+
+if not UNSPLASH_KEY:
+    raise EnvironmentError("Please create a .env.local file and add a Unsplash API key")
+
+app = Flask(__name__)
+
+
+@app.route("/new-image")
+def new_image():
+    word = request.args.get("query")
+    headers = {"Authorization": f"Client-ID {UNSPLASH_KEY}", "Accept-Version": "v1"}
+    payload = {"query": word}
+    response = rq.get(url=UNSPLASH_URL, headers=headers, params=payload)
+    return response.json()
