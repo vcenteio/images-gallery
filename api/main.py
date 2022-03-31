f2ae41a1503ef6453cb4db118a537cf742810664
Vitor Centeio <vitor.centeio@gmail.com>
2022-03-31 20:39:31 +0100

    feat(api-main): implement /images endpoint with support for GET and POST methods

--- api/main.py
+++ api/main.py
@@ -1,12 +1,20 @@
 import os
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests as rq
 from dotenv import load_dotenv
 
-load_dotenv("./.env.local")
+from mongo_client import mongo_client
 
+
+# create gallery database
+gallery = mongo_client.gallery
+# create images collection inside gallery database
+images_collection = gallery.images
+
+
+load_dotenv("./.env.local")
 UNSPLASH_URL = "https://api.unsplash.com/photos/random"
 UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY")
 
@@ -29,3 +37,16 @@ def new_image():
     payload = {"query": word}
     response = rq.get(url=UNSPLASH_URL, headers=headers, params=payload)
     return response.json()
+
+
+@app.route("/images", methods=["GET", "POST"])
+def images():
+    if request.method == "GET":
+        images = images_collection.find({})
+        return jsonify([image for image in images])
+    elif request.method == "POST":
+        # convert request payload to dict
+        image = request.get_json()  # assume request payload is json
+        image["_id"] = image.pop("id")
+        inserted_id = images_collection.insert_one(image).inserted_id
+        return {"inserted_id": inserted_id}
